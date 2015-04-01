fa5cea57e4e69c1ea9de9b0c29a1d2f1c04e096d
Miguel <miguel@debian.hsd1.ut.comcast.net>
2015-04-01 17:50:24 -0600

    added macros to initialize and delete persons

--- personsBirthday.c
+++ personsBirthday.c
@@ -2,38 +2,45 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/types.h>
-#include <linux/String.h>
+#include <linux/string.h>
 
 
 struct birthday {
 	int day;
 	int month;
 	int year;
-	String name;
+	string name;
 	struct list_head list;
 };
-
-void main (void)
+/* This function is called when the module is loaded. */
+int init_persons(void)
 {
-static LIST_HEAD(birthday_list);
-struct birthday *person;
+    printk(KERN_INFO "Loading Person 1\n");
+    static LIST_HEAD(birthday_list);
+	struct birthday *person;
 
-person = kmalloc(sizeof(*person),GFP_KERNEL);
-person->day = 2;
-person->month = 8;
-person->year = 1995;
-person->name ="name"
+	person = kmalloc(sizeof(*person),GFP_KERNEL);
+	person->day = 2;
+	person->month = 8;
+	person->year = 1995;
+	person->name = "test"
 
+    return 0;
+}
+
+/* This function is called when the module is removed. */
+void delete_persons(void) {
+	delete *person;
+	printk(KERN_INFO "Deleting Person\n");
+}
+
+/* Macros for registering module initilalize and tail pointers. */
 INIT_LIST_HEAD(&person->list);
 list_add_tail(&person->list, &birthday_list);
+/* Macros for registering module creating and deleting persons. */
+module_init( init_persons );
+module_exit( delete_persons );
 
-}
-
-void remove()
-{
-
-	//testing
-}
-
-void display ()
-{}
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Simple Module");
+MODULE_AUTHOR("SGG");

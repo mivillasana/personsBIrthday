b3564fd3c2b02b677fe6e7e1670b78f911e322b6
mivillasana <m_v032007@sbcglobal.net>
2015-04-01 13:14:51 -0700

    added string

--- personsBirthday.c
+++ personsBirthday.c
@@ -2,11 +2,14 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/types.h>
+#include <linux/String.h>
+
 
 struct birthday {
 	int day;
 	int month;
 	int year;
+	String name;
 	struct list_head list;
 };
 
@@ -19,6 +22,7 @@ person = kmalloc(sizeof(*person),GFP_KERNEL);
 person->day = 2;
 person->month = 8;
 person->year = 1995;
+person->name ="name"
 
 INIT_LIST_HEAD(&person->list);
 list_add_tail(&person->list, &birthday_list);
@@ -26,7 +30,10 @@ list_add_tail(&person->list, &birthday_list);
 }
 
 void remove()
-{}
+{
+
+	//testing
+}
 
 void display ()
 {}

98844d5be4a2e0bd7be65e67b4c2e61998427225
mivillasana <m_v032007@sbcglobal.net>
2015-04-02 08:43:50 -0700

    added the instantiation of 5 persons, no names yet

--- personsBirthday.c
+++ personsBirthday.c
@@ -2,28 +2,63 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/types.h>
-#include <linux/string.h>
 
 
 struct birthday {
 	int day;
 	int month;
 	int year;
-	string name;
+	char *name;
 	struct list_head list;
 };
+
+struct birthday *person;
+struct list_head *pos;
+
 static LIST_HEAD(birthday_list);
 /* This function is called when the module is loaded. */
 int init_persons(void)
 {
-    printk(KERN_INFO "Loading Person 1\n");
-	struct birthday *person;
-
+	int day, month, year;
+	char *name;
 	person = kmalloc(sizeof(*person),GFP_KERNEL);
 	person->day = 2;
 	person->month = 8;
 	person->year = 1995;
 	person->name = "test"
+
+	INIT_LIST_HEAD(&person->list);
+	printk(KERN_INFO "Loading Person 1\n");
+
+	while(i<5)
+	{
+
+		ptr = kmalloc(sizeof(*ptr),GFP_KERNEL);
+		get_random_bytes(&day, 1);
+	    ptr->day = day % 31;
+	    if(ptr->day < 0)
+	        ptr->day = ptr->day * -1;
+
+	    get_random_bytes(&month, 1);
+	    ptr->month = month % 12;
+
+	    if(ptr->month < 0)
+	        ptr->month = ptr->month * -1;
+
+	    get_random_bytes(&year, 1);
+	    ptr->year = (year % 2000)+1900;
+
+	    if(ptr->year < 0)
+	        ptr->year = ptr->year * -1;
+
+	    list_add(&(ptr->list), &person->list);
+    }
+	
+	printk(KERN_INFO "Traversing the list:)\n");
+    list_for_each(pos, &person->list){
+        tmp = list_entry(pos, struct birthday, list);
+        printk("day = %d month = %d year = %d\n", tmp->day, tmp->month, tmp->year);
+    }
 
     return 0;
 }
@@ -34,8 +69,7 @@ void delete_persons(void) {
 	printk(KERN_INFO "Deleting Person\n");
 }
 
-/* Macros for registering module initilalize and tail pointers. */
-INIT_LIST_HEAD(&person->list);
+
 list_add_tail(&person->list, &birthday_list);
 /* Macros for registering module creating and deleting persons. */
 module_init( init_persons );

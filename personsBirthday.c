5ee9d74c2de73025e4fea117cc49f618a85e3be0
Miguel <miguel@debian.hsd1.ut.comcast.net>
2015-04-02 11:20:46 -0600

    adding the persons works, the name is still a bit buggy

--- personsBirthday.c
+++ personsBirthday.c
@@ -2,7 +2,7 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/types.h>
-
+#include <linux/random.h>
 
 struct birthday {
 	int day;
@@ -12,28 +12,29 @@ struct birthday {
 	struct list_head list;
 };
 
-struct birthday *person;
+struct birthday *person, *tmp, *ptr;
 struct list_head *pos;
 
-static LIST_HEAD(birthday_list);
 /* This function is called when the module is loaded. */
-int init_persons(void)
+int person_init(void)
 {
 	int day, month, year;
+	int i = 0;
 	char *name;
 	person = kmalloc(sizeof(*person),GFP_KERNEL);
 	person->day = 2;
 	person->month = 8;
 	person->year = 1995;
-	person->name = "test"
+	person->name = "test";
 
 	INIT_LIST_HEAD(&person->list);
-	printk(KERN_INFO "Loading Person 1\n");
+	printk(KERN_INFO "Loading Persons:\n");
 
 	while(i<5)
 	{
 
 		ptr = kmalloc(sizeof(*ptr),GFP_KERNEL);
+		ptr->name = "person "+i;
 		get_random_bytes(&day, 1);
 	    ptr->day = day % 31;
 	    if(ptr->day < 0)
@@ -46,33 +47,37 @@ int init_persons(void)
 	        ptr->month = ptr->month * -1;
 
 	    get_random_bytes(&year, 1);
-	    ptr->year = (year % 2000)+1900;
+	    ptr->year = (year % 99);
 
 	    if(ptr->year < 0)
 	        ptr->year = ptr->year * -1;
 
 	    list_add(&(ptr->list), &person->list);
+	    i = i +1;
     }
 	
-	printk(KERN_INFO "Traversing the list:)\n");
+	printk(KERN_INFO "Beginning to traverse the list:\n");
     list_for_each(pos, &person->list){
         tmp = list_entry(pos, struct birthday, list);
-        printk("day = %d month = %d year = %d\n", tmp->day, tmp->month, tmp->year);
+        printk("%s birthday is %d/%d/%d\n",tmp->name, tmp->month, tmp->day, tmp->year);
     }
 
     return 0;
 }
 
 /* This function is called when the module is removed. */
-void delete_persons(void) {
-	delete *person;
-	printk(KERN_INFO "Deleting Person\n");
+void delete_persons(void) 
+{
+	list_for_each_entry(ptr, tmp, &person->list,list)
+	{
+		list_del(&ptr->list);
+		kfree(ptr);
+	}
+	printk(KERN_INFO "Deleting Persons!\n");
 }
 
-
-list_add_tail(&person->list, &birthday_list);
 /* Macros for registering module creating and deleting persons. */
-module_init( init_persons );
+module_init( person_init );
 module_exit( delete_persons );
 
 MODULE_LICENSE("GPL");

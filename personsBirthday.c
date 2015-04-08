7d572b43226a7ea9f91a5f9ab8d824cf948d6414
Miguel <miguel@debian.hsd1.ut.comcast.net>
2015-04-08 08:02:21 -0700

    "Final version"

--- personsBirthday.c
+++ personsBirthday.c
@@ -3,6 +3,7 @@
 #include <linux/kernel.h>
 #include <linux/types.h>
 #include <linux/random.h>
+#include <linux/string.h>
 
 struct birthday {
 	int day;
@@ -13,19 +14,19 @@ struct birthday {
 };
 
 struct birthday *person, *tmp, *ptr;
-struct list_head *pos;
+struct list_head *pos; // pos points to the head
 
 /* This function is called when the module is loaded. */
 int person_init(void)
 {
 	int day, month, year;
 	int i = 0;
-	char *name;
+	char *name = "Person";
 	person = kmalloc(sizeof(*person),GFP_KERNEL);
 	person->day = 2;
 	person->month = 8;
 	person->year = 1995;
-	person->name = "test";
+	person->name = "Person";
 
 	INIT_LIST_HEAD(&person->list);
 	printk(KERN_INFO "Loading Persons:\n");
@@ -34,32 +35,37 @@ int person_init(void)
 	{
 
 		ptr = kmalloc(sizeof(*ptr),GFP_KERNEL);
-		ptr->name = "person "+i;
+		ptr->name = name;
 		get_random_bytes(&day, 1);
 	    ptr->day = day % 31;
 	    if(ptr->day < 0)
 	        ptr->day = ptr->day * -1;
-
+		if(ptr->day == 0)
+	    	ptr->day = ptr->day + 1;
 	    get_random_bytes(&month, 1);
 	    ptr->month = month % 12;
 
 	    if(ptr->month < 0)
 	        ptr->month = ptr->month * -1;
+	    if(ptr->month == 0)
+	    	ptr->month = ptr->month + 1;
 
 	    get_random_bytes(&year, 1);
 	    ptr->year = (year % 99);
 
 	    if(ptr->year < 0)
 	        ptr->year = ptr->year * -1;
-
+	    if(ptr->year < 50)
+	    	ptr->year = ptr->year + 50;
 	    list_add(&(ptr->list), &person->list);
 	    i = i +1;
     }
-	
+	i=0;
 	printk(KERN_INFO "Beginning to traverse the list:\n");
     list_for_each(pos, &person->list){
         tmp = list_entry(pos, struct birthday, list);
-        printk("%s birthday is %d/%d/%d\n",tmp->name, tmp->month, tmp->day, tmp->year);
+        i = i+1;
+        printk("%s %d birthday is %d/%d/%d\n",tmp->name,i,tmp->month, tmp->day, tmp->year);
     }
 
     return 0;
@@ -68,12 +74,14 @@ int person_init(void)
 /* This function is called when the module is removed. */
 void delete_persons(void) 
 {
-	list_for_each_entry(ptr, tmp, &person->list,list)
+	int i = 0;
+	list_for_each_entry_safe(ptr, tmp, &person->list,list)
 	{
-		list_del(&ptr->list);
+		i = i +1;
+		printk(KERN_INFO "Deleting %s %d birthday %d/%d/%d\n",ptr->name,i,ptr->month, ptr->day, ptr->year);		list_del(&ptr->list);
 		kfree(ptr);
 	}
-	printk(KERN_INFO "Deleting Persons!\n");
+	printk(KERN_INFO "Deleted all Persons!\n");
 }
 
 /* Macros for registering module creating and deleting persons. */
